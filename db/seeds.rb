0edc9ee2282918777fb0c3b5f3355666775207ee
SHIRIN NAGLE <g00363458@gmit.ie>
2021-05-02 11:23:59 +0000

    added review to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 more_albums = [
     {:artist => 'Radiohead',:title =>'Ok Computer',
-    :rating =>'5', :release_date => '10-May-1996'},
+    :review => '',:rating =>'5', :release_date => '10-May-1996'},
     {:artist => 'The Pixies',:title =>'Doolittle',
-    :rating =>'5', :release_date => '17-Mar-1990'},
+    :review => '',:rating =>'5', :release_date => '17-Mar-1990'},
     {:artist => 'Aldous Harding',:title =>'Designer',
-    :rating =>'5', :release_date => '30-Apr-2018'},
+    :review => '',:rating =>'5', :release_date => '30-Apr-2018'},
     {:artist => 'Amy Winehouse',:title =>'Back To black',
-    :rating =>'4', :release_date => '12-Sep-2006'}
+    :review => 'Almost perfect',:rating =>'4', :release_date => '12-Sep-2006'}
 ]
 
 more_albums.each do |album|
